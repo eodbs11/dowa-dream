cad0f99cf163024bcc89b50bae9234e6e5b50af2
eodbs11 <152463186+eodbs11@users.noreply.github.com>
2024-04-03 15:10:35 +0900

    Initial commit

---
+++ src/main/java/com/dowa/dream/DowaDreamApplication.java
@@ -0,0 +1,13 @@
+package com.dowa.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowaDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DowaDreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dowa/dream/DowaDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dowa.dream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowaDreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
